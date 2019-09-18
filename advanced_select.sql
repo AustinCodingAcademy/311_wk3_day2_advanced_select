82624eedc2fa739aeb356649bb326af2a5a5cca8
jaywiles <jay.wiles@gmail.com>
2019-09-17 19:23:36 -0500

    Jay Wiles - one change and done

--- advanced_select.sql
+++ advanced_select.sql
@@ -1,22 +1,22 @@
 -- #3 below
-SELECT
-    MIN(users.first_name) as userName,
-    usersAddress.county as userCounty,
-    count(usersAddress.county) as userCount
-FROM
-	users
-JOIN
-	usersAddress
-WHERE
-	users.id = usersAddress.user_id
-GROUP BY
-    userCounty
-HAVING
-	userCount > 10
+-- SELECT
+--     MIN(users.first_name) as userName,
+--     usersAddress.county as userCounty,
+--     count(usersAddress.county) as userCount
+-- FROM
+-- 	users
+-- JOIN
+-- 	usersAddress
+-- WHERE
+-- 	users.id = usersAddress.user_id
+-- GROUP BY
+--     userCounty
+-- HAVING
+-- 	userCount > 10
 
 
 
--- #2 below
+#2 below
 -- SELECT
 --     SUBSTR(phone1, 1, 3) as areaCode,
 --     count(phone1) as areaCodeCount
@@ -26,6 +26,7 @@ HAVING
 --     SUBSTR(phone1, 1, 3)
 -- ORDER BY
 -- 	areaCodeCount DESC
+--     limit 1
 
     
 -- #1 below
